6ad159671537d3c2ec2e00fc0089f4fc3610af80
darianzhang0 <77823043+darianzhang0@users.noreply.github.com>
2023-05-25 22:32:54 -0400

    Update main.py

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -14,17 +14,12 @@ c = conn.cursor()
 
 def web_scraper() :
   job_searcher.jobsearch()
-  #print(list_of_jobs)
 
   # runs comparison on jobs and given list of keywords
   job_comparer.description_query(resume)
 
 
   # adds lists from list_of_jobs to a database called job_postings.db
-
-  #c.execute('''DROP TABLE IF EXISTS job_postings''')
-  #c.execute('''CREATE TABLE job_postings(position TEXT, company_name TEXT, location TEXT, link TEXT, rating INT)''')
-
   for job in list_of_jobs:
     c.execute('''SELECT * FROM job_postings WHERE position = "{}" AND company_name = "{}" AND location = "{}" AND link = "{}"'''.format(job[0],job[1],job[2],job[3]))
     query = c.fetchone()
@@ -32,13 +27,8 @@ def web_scraper() :
       c.execute('''INSERT INTO job_postings VALUES(?,?,?,?,?)''', (job[0], job[1], job[2], job[3],job[4]))
       conn.commit()
 
-  #c.execute('''SELECT * FROM job_postings''')
-  #print(c.fetchall())
-
 # Returns the top n results, sorted by the rating column
 def top(n) :
-  #c.execute('''SELECT * FROM job_postings ORDER BY rating DESC LIMIT {}'''.format(n))  
-  #print(c.fetchall())
   print(pd.read_sql_query('''SELECT * FROM job_postings ORDER BY rating DESC LIMIT {}'''.format(n), conn))
 
 #Commands to run web_scraper daily
